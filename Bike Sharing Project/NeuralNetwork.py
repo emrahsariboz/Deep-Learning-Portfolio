e45e550495804001a2b7a4280007068e766fb1b6
Emrah <emrahsarboz@gmail.com>
2020-06-14 00:58:12 -0600

    UnitTests

--- Bike Sharing Project/NeuralNetwork.py
+++ Bike Sharing Project/NeuralNetwork.py
@@ -13,7 +13,7 @@ class neural_network:
         self.output_nodes = output_nodes
         self.learning_rate = learning_rate
         
-        self.acti_func = lambda x: ( 1 / ( 1 + np.exp(-x)))
+     
         
         self.weights_i_h = np.random.normal(0.0, self.input_nodes**-0.5, 
                                        (self.input_nodes, self.hidden_nodes))
@@ -22,6 +22,9 @@ class neural_network:
                                             (self.hidden_nodes, self.output_nodes))
         
         self.sigmoid_output_derivative = lambda x: (x * (1 - x))
+    
+    def activation_function(self, x):
+        return ( 1 / ( 1 + np.exp(-x)))
 
     def train(self, features, target):
         
@@ -31,40 +34,37 @@ class neural_network:
         
         for X, y in zip(features, target):
             final_outputs, hidden_outputs = self.forward_pass(X)
-            delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y, 
-                                                                        delta_weights_i_h, delta_weights_h_o)
+            delta_weights_i_h, delta_weights_h_o = self.backpropagation(final_outputs, hidden_outputs, X, y, delta_weights_i_h, delta_weights_h_o)
         self.update_weights(delta_weights_i_h, delta_weights_h_o, n_records)
         
-    def MSE(y, Y):
+    def MSE(self, y, Y):
         return np.mean((y-Y)**2)
     
     
     def forward_pass(self, X):
         
         hidden_input= np.dot(X, self.weights_i_h)
-        hidden_output = self.acti_func(hidden_input)
+        hidden_output = self.activation_function(hidden_input)
         
         
         #Output layer
         
         final_inputs = np.dot(hidden_output, self.weights_h_o)
-        final_outputs = self.acti_func(final_inputs)
+        final_outputs = self.activation_function(final_inputs)
         
         return final_outputs, hidden_output
     
-    def backpropagation(self, final_outputs, hidden_outputs, X, y, delta_weights_i_h, 
-                        delta_weights_h_o):
+    def backpropagation(self, final_outputs, hidden_outputs, X, y, delta_weights_i_h, delta_weights_h_o):
         
-        error = self.MSE(y - final_outputs)
+        error = y - final_outputs
         
-        output_error_term = np.dot(error, self.weights_h_o)
-        output_error_delta = output_error_term * self.sigmoid_output_derivative(error)
-        
-        hidden_error_term = np.dot(output_error_delta, self.weights_i_h)
+        output_error_term = error
+
+        hidden_error_term = output_error_term.dot(self.weights_h_o)
         hidden_error_delta = hidden_error_term * self.sigmoid_output_derivative(output_error_term)
         
-        delta_weights_h_o += np.dot(final_outputs, output_error_delta)
-        delta_weights_i_h += np.dot(hidden_outputs, hidden_error_delta)
+        delta_weights_h_o += np.dot(hidden_nodes, output_error_delta)
+        delta_weights_i_h += np.dot(self.input_nodes, hidden_error_delta)
         
         return delta_weights_i_h, delta_weights_h_o
     
